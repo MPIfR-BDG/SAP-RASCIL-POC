7b10116154c991f32e8b228b4150637a0c5200f6
Gianluca De Lorenzo <gianluca.de.lorenzo@sap.com>
2020-07-16 17:10:25 +0200

    rascil_create_image_from_vis operator prepared for testing

--- vflow/subengines/com/sap/python36/operators/rascil_create_image_from_vis/script.py
+++ vflow/subengines/com/sap/python36/operators/rascil_create_image_from_vis/script.py
@@ -3,33 +3,46 @@ from rascil.data_models.polarisation import PolarisationFrame
 from rascil.processing_components.imaging import create_image_from_visibility
 
 
-log = api.logger
-log.info("Starting model creation operator")
+def wrapper(api):
+
+    log = api.logger
+    log.info("Starting model creation operator")
 
 
-def execute(vis_pickle, advice_pickle):
-    log.debug("Executing model creation")
-    vis_list = pickle.loads(vis_pickle)
-    advice = pickle.loads(advice_pickle)
-    vis_slices = advice['vis_slices']
-    npixel = advice['npixels2']
-    cellsize = advice['cellsize']
-    model_list = []
-    for vis in vis_list:
-        model_list.append(
-            create_image_from_visibility(
-                vis,
-                npixel=npixel,
-                frequency=[vis.frequency[0]],
-                channel_bandwidth=[vis.channel_bandwidth[0]],
-                cellsize=cellsize,
-                phasecentre=vis.phasecentre,
-                polarisation_frame=PolarisationFrame("stokesI")
+    def execute(vis_pickle, advice_pickle):
+        log.debug("Executing model creation")
+        vis_list = pickle.loads(vis_pickle)
+        advice = pickle.loads(advice_pickle)
+        vis_slices = advice['vis_slices']
+        npixel = advice['npixel']
+        cellsize = advice['cellsize']
+        model_list = []
+        for vis in vis_list:
+            model_list.append(
+                create_image_from_visibility(
+                    vis,
+                    npixel=npixel,
+                    frequency=[vis.frequency[0]],
+                    channel_bandwidth=[vis.channel_bandwidth[0]],
+                    cellsize=cellsize,
+                    phasecentre=vis.phasecentre,
+                    polarisation_frame=PolarisationFrame("stokesI")
+                )
             )
-        )
-    api.send("output", pickle.dumps(model_list))
+        api.send("output", pickle.dumps(model_list))
 
 
-api.add_shutdown_handler(lambda: log.info(
-    "Shutting down model creation operator"))
-api.set_port_callback(["inputvis", "inputadvice"], execute)
+    #api.add_shutdown_handler(lambda: log.info(
+        #"Shutting down model creation operator"))
+    api.set_port_callback(["inputvis", "inputadvice"], execute)
+
+
+# ////////////////////////////////////////////////////////
+
+try:
+    api
+except NameError:
+    if __name__ == "__main__":
+        print("Error: No api object found")
+else:
+    wrapper(api)
