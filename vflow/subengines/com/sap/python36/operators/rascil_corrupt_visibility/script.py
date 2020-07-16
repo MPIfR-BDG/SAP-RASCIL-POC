0b703a8494fcaf9dc94afd85b697a89ecd4ff954
Gianluca De Lorenzo <gianluca.de.lorenzo@sap.com>
2020-07-16 16:51:58 +0200

    rascil_corrupt_visibility operator prepared for testing

--- vflow/subengines/com/sap/python36/operators/rascil_corrupt_visibility/script.py
+++ vflow/subengines/com/sap/python36/operators/rascil_corrupt_visibility/script.py
@@ -2,19 +2,30 @@ import pickle
 from rascil.workflows.serial.simulation.simulation_serial import (
     corrupt_list_serial_workflow)
 
-log = api.logger
-log.info("Starting visibility corruption operator")
 
+def wrapper(api):
+    log = api.logger
+    log.info("Starting visibility corruption operator")
 
-def execute(vis_pickle):
-    log.debug("Executing visibility corruption")
-    vis_list = pickle.loads(vis_pickle)
-    corrupted_vislist = corrupt_list_serial_workflow(
-        vis_list,
-        phase_error=api.config.phase_error)
-    api.send("output", pickle.dumps(corrupted_vislist))
+    def execute(vis_pickle):
+        log.debug("Executing visibility corruption")
+        vis_list = pickle.loads(vis_pickle)
+        corrupted_vislist = corrupt_list_serial_workflow(
+            vis_list,
+            phase_error=api.config.phase_error)
+        api.send("output", pickle.dumps(corrupted_vislist))
 
+    #api.add_shutdown_handler(lambda: log.info(
+        #"Shutting down visibility corruption operator"))
 
-api.add_shutdown_handler(lambda: log.info(
-    "Shutting down visibility corruption operator"))
-api.set_port_callback("input", execute)
+    api.set_port_callback("input", execute)
+
+# ////////////////////////////////////////////////////////
+
+try:
+    api
+except NameError:
+    if __name__ == "__main__":
+        print("Error: No api object found")
+else:
+    wrapper(api)
