d43432579b8a0b1986c68aace98e65b87c970a03
Gianluca De Lorenzo <gianluca.de.lorenzo@sap.com>
2020-07-22 08:46:23 +0000

    first e2e running version of the RASCIL demo graph

--- vflow/subengines/com/sap/python36/operators/rascil_deconvolve/script.py
+++ vflow/subengines/com/sap/python36/operators/rascil_deconvolve/script.py
@@ -1,6 +1,25 @@
+import json
 import pickle
+import numpy as np
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+from rascil.processing_components.visibility import (
+    convert_blockvisibility_to_visibility as convert_bvis_to_vis)
+from rascil.workflows.serial.simulation.simulation_serial import (
+    simulate_list_serial_workflow)
+from rascil.processing_components.imaging import advise_wide_field
+from rascil.data_models.polarisation import PolarisationFrame
+from rascil.processing_components.simulation import (
+    create_low_test_image_from_gleam)
 from rascil.workflows.serial.imaging.imaging_serial import (
-    deconvolve_list_serial_workflow)
+    predict_list_serial_workflow)
+from rascil.workflows.serial.imaging.imaging_serial import invert_list_serial_workflow
+from rascil.data_models.polarisation import PolarisationFrame
+from rascil.processing_components.imaging import create_image_from_visibility
+from rascil.workflows.serial.simulation.simulation_serial import (
+    corrupt_list_serial_workflow)
+from rascil.workflows.serial.imaging.imaging_serial import deconvolve_list_serial_workflow
+
 
 
 def wrapper(api):
